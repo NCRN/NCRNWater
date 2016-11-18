378366b730a02fd908b6ee3a1cca164073fd1070
John Paul Schmit <jschmit@nps.gov>
2016-11-18 14:39:08 -0500

    v 0.22.6.2  Test commit
    - Has updates to documentation of waterseries().

--- R/waterseries.R
+++ R/waterseries.R
@@ -20,16 +20,16 @@
 #' \item{"site}{If more than one site is included in the input object, this will produce a different line for each site.}
 #' \item{"park"}{If more than one park is included in the input object, this will produce a different line for each park}
 #' \item{"char"}{If more than one characteristic is included in the input object, this will produce a different line for each one} 
-#' \item{"none}{All data will be displayed as a single ine. Used if you only have data from a single charactersitic from a single site}
+#' \item{"none}{All data will be displayed as a single line. Used if you only have data from a single charactersitic from a single site}
 #' }
 #' 
-#' @param assesment Vector indicating if assesment lines will be marked on the graph. See details below.
+#' @param assessment Vector indicating if assesment lines will be marked on the graph. See details below.
 #' @param layers Defaults to c("points","line") Indicates which layers you wish to see on the plot. 
 #' @param yname Text, defaults to \code{NA}. A label for the y-axis. If an \code{Characteristic}, \code{Site}, or \code{Park} object is passed to \code{object}, then the y-label will default to the Display Name and Units for the Charecteristic, unless overwitten by the \code{yname} argument. If a \code{data.frame} is passed then the y-label will either be the text from \code{yname} or blank if \code{yname} is left as \code{NA}.
 #' @param xname Text, defaults to \code{NA}. A label for the x-axis. If a \code{Characteristic}, \code{Site}, or \code{Park} object is passed to \code{object}, then the x-label will default to whatever is indicated in \code{by}.unless overwitten by the \code{xname} argument. If a \code{data.frame} is passed then the x-label will either be the text from \code{xname} or blank if \code{xname} is left as \code{NA}.
 #' 
 #' @param labels A character vector indicating the labels for the data series, defaults to NA. If labels are provided (one for each series) they will be printed. If \code{object} is a \code{data.frame} and \code{labels} is \code{NA} then no labels will be printed. If \code{object} is a \code{Characteristic}, \code{Site}, or \code{Park} object, and \code{labels} is \code{NA} then the default will depend on the \code{by} argument. "site" willbe labled with the site name from the \code{Site}'s \code{Display Name} slot and "park" with the \code{Park}'s short name from the \code{ShortName} slot. 
-#' @param title A title in the graph in quotes. Defauls to \code{NULL}, which indicates no title should be used. 
+#' @param title A title in the graph in quotes. Defaults to \code{NULL}, which indicates no title should be used. 
 #' @param colors a length 3 character vector with the colors for the points, line connecting the points and assessment lines. 
 #' @param sizes a length 3 numeric vector with the sizes for the points, line connecting the points and assessment lines.
 #' 
@@ -37,7 +37,7 @@
 #' 
 #' @return Creates a time series plot.
 #' 
-#' @details  The \code{assessment} argument determines if lines representing the assessment values should be drawn on the graph. If \code{FALSE} then no lines will be drawn. If \code{TRUE}, the default, then the upper and lower points indicated in \code{object}'s \code{Character} objects will be used to draw the lines. Note that if there are multiple assessemnt points, for example if diffrernt parks have different points, or if there is both an upper and lower point, they will all be drawn. If a \code{vector} of numbers is passed to \code{assessemnt} instead then those will serve as the assessment values and lines will be drawn accordingly. Note that if \code{obejct} is a \code{data.frame} then the only way to draw assessment lines is by passing a numeric \code{vector} to \code{assessment}.
+#' @details  The \code{assessment} argument determines if lines representing the assessment values should be drawn on the graph. If \code{FALSE} then no lines will be drawn. If \code{TRUE}, the default, then the upper and lower points indicated in \code{object}'s \code{Character} objects will be used to draw the lines. Note that if there are multiple assessment points, for example if diffrerent parks have different points, or if there is both an upper and lower point, they will all be drawn. If a \code{vector} of numbers is passed to \code{assessemnt} instead then those will serve as the assessment values and lines will be drawn accordingly. Note that if \code{obejct} is a \code{data.frame} then the only way to draw assessment lines is by passing a numeric \code{vector} to \code{assessment}.
 #' 
 #' @export
 
