5bede7eb9abd796937fd6df26f2ebb458080245d
johnpauls <johnpauls@operamail.com>
2016-11-22 13:38:52 -0500

    v 0.23 more waterseries
    -finished update of watersereis
    -exposed more graphics choices to the API

--- R/waterseries.R
+++ R/waterseries.R
@@ -2,7 +2,7 @@
 #' @include getWData.R
 #' @importFrom magrittr %>% 
 #' @importFrom lubridate year month
-#' @importFrom ggplot2 ggplot aes geom_point labs theme_bw theme element_blank geom_hline geom_line scale_color_discrete scale_shape_manual scale_x_continuous
+#' @importFrom ggplot2 ggplot aes geom_point geom_line scale_color_manual scale_shape_manual geom_hline labs theme_bw theme element_blank
 #' @importFrom plotly ggplotly config
 #' @importFrom viridis scale_color_viridis
 
@@ -20,21 +20,21 @@
 #' \describe{
 #' \item{"site}{If more than one site is included in the input object, this will produce a different line for each site.}
 #' \item{"park"}{If more than one park is included in the input object, this will produce a different line for each park}
-#' \item{"char"}{If more than one characteristic is included in the input object, this will produce a different line for each one} 
-#' \item{"none}{All data will be displayed as a single line. Used if you only have data from a single charactersitic from a single site}
+#' \item{"char"}{The defauls. If more than one characteristic is included in the input object, this will produce a different line for each one} 
 #' }
 #' 
 #' @param assessment Vector indicating if assesment lines will be marked on the graph. See details below.
 #' @param layers Defaults to c("points","line") Indicates which layers you wish to see on the plot. 
 #' @param yname Text, defaults to \code{NA}. A label for the y-axis. If an \code{Characteristic}, \code{Site}, or \code{Park} object is passed to \code{object}, then the y-label will default to the Display Name and Units for the Charecteristic, unless overwitten by the \code{yname} argument. If a \code{data.frame} is passed then the y-label will either be the text from \code{yname} or blank if \code{yname} is left as \code{NA}.
-#' @param xname Text, defaults to \code{NA}. A label for the x-axis. If a \code{Characteristic}, \code{Site}, or \code{Park} object is passed to \code{object}, then the x-label will default to whatever is indicated in \code{by}.unless overwitten by the \code{xname} argument. If a \code{data.frame} is passed then the x-label will either be the text from \code{xname} or blank if \code{xname} is left as \code{NA}.
+#' @param xname Text, defaults to \code{NA}. A label for the x-axis. If a \code{Characteristic}, \code{Site}, or \code{Park} object is passed to \code{object}, then the x-label will default to whatever is indicated in \code{by}, unless overwitten by the \code{xname} argument. If a \code{data.frame} is passed then the x-label will either be the text from \code{xname} or blank if \code{xname} is left as \code{NA}.
 #' 
-#' @param labels A character vector indicating the labels for the data series, defaults to NA. If labels are provided (one for each series) they will be printed. If \code{object} is a \code{data.frame} and \code{labels} is \code{NA} then no labels will be printed. If \code{object} is a \code{Characteristic}, \code{Site}, or \code{Park} object, and \code{labels} is \code{NA} then the default will depend on the \code{by} argument. "site" willbe labled with the site name from the \code{Site}'s \code{Display Name} slot and "park" with the \code{Park}'s short name from the \code{ShortName} slot. 
+#' @param labels A character vector indicating the labels for the data series, defaults to NA. If labels are provided (one for each series) they will be printed. If \code{object} is a \code{data.frame} and \code{labels} is \code{NA} then no labels will be printed. If \code{object} is a \code{Characteristic}, \code{Site}, or \code{Park} object, and \code{labels} is \code{NA} then the default will depend on the \code{by} argument. "site" willbe labeled with the site name from the \code{Site}'s \code{Display Name} slot and "park" with the \code{Park}'s short name from the \code{ShortName} slot. 
 #' @param title A title in the graph in quotes. Defaults to \code{NULL}, which indicates no title should be used. 
 #' @param colors a character vector with the colors for the points and lines. 
 #' @param assesscolor a length one charactter vector with the color for the assessment lines.
 #' @param sizes a length 3 numeric vector with the sizes for the points, line connecting the points and assessment lines.
-#' 
+#' @param legend  a vector indicating where the legend position. Can be: "none","left","right","top","bottom" or a two element numeric vector.
+#' @param webplot  If TRUE, the plot is produced using ggploty from the ploty package. Will produce a html plot with interactive features.
 #' @param ... Additional arguments used to select and filter data passed to \code{\link{getWData}}
 #' 
 #' @return Creates a time series plot.
@@ -43,12 +43,12 @@
 #' 
 #' @export
 
-setGeneric(name="waterseries",function(object,parkcode=NA, sitecode=NA,charname, by="char",assessment=TRUE,layers=c("points","line"), xname=NA,yname=NA,labels=NA,title=NULL,colors=NA, assesscolor="red", sizes=c(3, 0.8, 1.1), webplot=FALSE,...){standardGeneric("waterseries")},signature=c("object") )
+setGeneric(name="waterseries",function(object,parkcode=NA, sitecode=NA,charname, by="char",assessment=TRUE,layers=c("points","line"), xname=NA,yname=NA,labels=NA,title=NULL,colors=NA, assesscolor="red", sizes=c(3, 0.8, 1.1), legend="bottom",webplot=FALSE,...){standardGeneric("waterseries")},signature=c("object") )
 
 
 setMethod(f="waterseries", signature=c(object="NCRNWaterObj"),
-          function(object,parkcode, sitecode, charname,by,assessment,xname,yname,labels,title,colors, assesscolor,sizes, webplot,...){
-            PlotData<-getWData(object=object,parkcode=parkcode, sitecode=sitecode, charname=charname,...)
+  function(object,parkcode, sitecode, charname,by,assessment,xname,yname,labels,title,colors, assesscolor,sizes,legend, webplot,...){
+          PlotData<-getWData(object=object,parkcode=parkcode, sitecode=sitecode, charname=charname,...)
             if(is.na(yname)) yname<-paste0(getCharInfo(object=object, charname=charname, info="DisplayName")," (",
                                            getCharInfo(object=object, charname=charname, info="Units"),")") %>% unique
             if(is.na(xname)) xname<-"Date"
@@ -64,40 +64,34 @@ setMethod(f="waterseries", signature=c(object="NCRNWaterObj"),
             
             assessment<-assessment[!is.na(assessment)] # needed if there is no upper or lower assessment.
             callGeneric(object=PlotData, by=by, assessment=assessment, layers=layers, xname=xname, yname=yname,labels=labels, 
-                        title=title,colors=colors, assesscolor=assesscolor, sizes=sizes, webplot=webplot)
+                        title=title,colors=colors, assesscolor=assesscolor, sizes=sizes,legend=legend, webplot=webplot)
           })
 
 setMethod(f="waterseries", signature=c(object="data.frame"),
-          function(object,by,assessment,layers, xname,yname,labels,title,colors,assesscolor, sizes, webplot){
+          function(object,by,assessment,layers, xname,yname,labels,title,colors,assesscolor, sizes,legend, webplot){
             Grouper<-switch(by,
-                            none="",
                             char=object$Characteristic %>% factor,
                             site=object$Site %>% factor,
                             park=object$Park %>% factor)
             if(all(is.na(yname))) yname<-""
             if(all(is.na(xname))) xname<-""
             if(all(is.na(labels))) labels<-switch(by,
-                                                  none="",
                                                   char=object$Characteristic %>% unique,
                                                   site=object$Site %>% unique,
                                                   park=object$Park %>% unique)
             
-           Xaxis<- object$Date#if(by=="year") month(object$Date,label=F) else object$Date
+           Xaxis<- object$Date
         
-            OutPlot<-ggplot(object,aes(Xaxis,Value,#color=Grouper,
-                                       shape=Grouper)) +
-              {if ("points" %in% layers) geom_point(aes(color=Grouper), size=sizes[1],pch=16) }+
-              {if ("line" %in% layers) geom_line(#color=colors, 
-                aes(color=Grouper),size=sizes[2])} +
-            {if (is.na(colors)) scale_color_viridis(name="legend",labels=labels, discrete = T)}+
+            OutPlot<-ggplot(object,aes(Xaxis,Value))+
+              {if ("points" %in% layers) geom_point(aes(color=Grouper, shape=Grouper), size=sizes[1]) }+
+              {if ("line" %in% layers) geom_line(aes(color=Grouper),size=sizes[2])} +
+              {if (is.na(colors)) scale_color_viridis(name="legend",labels=labels, discrete = T)}+
               {if (!is.na(colors))scale_color_manual(name="legend",labels=labels, values=colors)}+
-            #, values=colors) +
-              scale_shape_manual(name="legend",values=1:nlevels(Grouper),labels=labels) +
-           #   {if(by=="year") scale_x_continuous(name=xname,labels=Xaxis %>% unique %>% sort %>% month(T) %>% as.character, breaks=1:12)} +
+              scale_shape_manual(name="legend",  labels=labels, values=c(16,15,17,18,1,0,2,5,6,3,4,8,13,9,12)[1:nlevels(Grouper)]) +
               {if (is.numeric(assessment)) geom_hline(yintercept=assessment,color=assesscolor,linetype="dashed",size=sizes[3])} +
               labs(title=title,y=yname,x=xname) +
               theme_bw() +
-              theme(panel.grid = element_blank(), legend.title=element_blank(),legend.position=if(by=="none") "none" else "bottom")
+              theme(panel.grid = element_blank(), legend.title=element_blank(),legend.position=legend)
             
             ifelse(webplot, return(ggplotly(OutPlot) %>% plotly::config(displaylogo=F)),return(OutPlot))
 })
