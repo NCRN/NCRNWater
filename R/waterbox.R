a0774ef9f5940c6f366909a19717bb4bbfddd595
johnpauls <johnpauls@operamail.com>
2016-12-02 15:26:31 -0500

    v 0.23.1 improvements to waterbox
    - added in data subsetting by park and site to remove unneeded assessemnt lines
    - changed "points" to "assessment"
    - added colors for assessment, outliers
    - add sizes for line and points

--- R/waterbox.R
+++ R/waterbox.R
@@ -10,6 +10,8 @@
 #' 
 #' @description Produces box plots from water data. 
 #' 
+#' @inheritParams getChars
+#' 
 #' @param object Either a \code{data.frame} that is the output of \code{getWData}, a \code{Characteristic} object a \code{Site} object, a \code{Park} object or a \code{list} of such objects.
 #' @param charname Required if \code{object} is not a \code{data.frame}. Name, in quotes, of a single \code{Characteristic} whose data should be graphed.
 #' @param by Indicates how the data for the boxplot should be grouped. A text variable in quotes. Choices are:
@@ -19,27 +21,31 @@
 #' \item{"site}{If more than one site is included in the input object, this will produce a different box for each site.}
 #' \item{"park"}{If more than one park is included int he input object, theis will prodice a different box for each park}
 #' }
-#' @param points Vector indicating if assesment points will be maked on the graph. See details below.
+#' @param assessmemt Vector indicating if assessment points will be maked on the graph. See details below.
 #' @param yname Text, defaults to \code{NA}. A label for the y-axis. If an \code{Characteristic}, \code{Site}, or \code{Park} object is passed to \code{object}, then the y-label will default to the Display Name and Units for the Charecteristic, unless overwitten by the \code{yname} argument. If a \code{data.frame} is passed then the y-label will either be the text from \code{yname} or blank if \code{yname} is left as \code{NA}.
 #' @param xname Text, defaults to \code{NA}. A label for the x-axis. If a \code{Characteristic}, \code{Site}, or \code{Park} object is passed to \code{object}, then the x-label will default to whatever is indicated in \code{by}.unless overwitten by the \code{xname} argument. If a \code{data.frame} is passed then the x-label will either be the text from \code{xname} or blank if \code{xname} is left as \code{NA}.
 #' 
 #' @param labels A character vector indicating the labels for the bars, defaults to NA. If labels are provided (one for each bar) they will be printed. If \code{object} is a \code{data.frame} and \code{labels} is \code{NA} then no labels will be printed. If \code{object} is a \code{Characteristic}, \code{Site}, or \code{Park} object, and \code{labels} is \code{NA} then the default will depend on the \code{gropby} argument. "year" will be labeled with 4 digit years, "month" with 3 letter months (Jan,Feb, etc), "site" with the site name from the \code{Site}'s \code{Display Name} slot and "park" with the \code{Park}'s short name from the \code{ShortName} slot. 
 #' @param title A title in the graph in quotes. Defauls to \code{NULL}, which indicates no title should be used. 
+#' @param assesscolor a length one character vector with the color for the assessment lines.
+#' @param outliercolor a length one character vector with the color for the outlier points.
+#' @param sizes a length 3 numeric vector with the sizes for the outlier points, lines of the boxplot and assessment lines.
+#' @param webplot  If TRUE, the plot is produced using ggploty from the ploty package. Will produce a html plot with interactive features.
 #' 
 #' @param ... Additional arguments used to select and filter data passed to \code{\link{getWData}}
 #' 
 #' @return Creates a boxplot
 #' 
-#' @details  The \code{points} argument determines if lines representing the assessment points should be drawn on the graph. If \code{FALSE} then no lines will be drawn. If \code{TRUE}, the default, then the upper and lower points indicated in \code{object}'s \code{Character} objects will be used to draw the lines. Note that if there are multiple assessemnt points, for example if diffrernt parks have different points, or if htere is both an upper and lower point, they will all be drawn. If a \code{vector} of numbers is passed to \code{points} instead then those will serve as the assessment points and lines will be drawn accordingly. Note that if \code{obejct} is a \code{data.frame} then the only way to draw assessment points is by passing a \code{numeric vector} to \code{points}.
+#' @details  The \code{assessment} argument determines if lines representing the assessment points should be drawn on the graph. If \code{FALSE} then no lines will be drawn. If \code{TRUE}, the default, then the upper and lower assessment points indicated in \code{object}'s \code{Character} objects will be used to draw the lines. Note that if there are multiple assessemnt points, for example if diffrerent sites have different points, or if there is both an upper and lower point, they will all be drawn. If a \code{vector} of numbers is passed to \code{assessment} instead then those will serve as the assessment points and lines will be drawn accordingly. Note that if \code{obejct} is a \code{data.frame} then the only way to draw assessment points is by passing a \code{numeric vector} to \code{assessment}.
 #' 
 #' @export
 
 
-setGeneric(name="waterbox",function(object,charname, by="year",points=TRUE,yname=NA,xname=NA,labels=NA,title=NULL,webplot=FALSE,...){standardGeneric("waterbox")},signature=c("object") )
+setGeneric(name="waterbox",function(object,parkcode=NA, sitecode=NA, charname, by="year",assessment=TRUE,yname=NA,xname=NA,labels=NA,title=NULL,assesscolor="red", outliercolor="blue",sizes=c(2,.5,1), webplot=FALSE,...){standardGeneric("waterbox")},signature=c("object") )
 
 setMethod(f="waterbox", signature=c(object="NCRNWaterObj"),
-  function(object,charname,by,points,yname,xname,labels,title,webplot,...){
-   PlotData<-getWData(object=object,charname=charname,...)
+  function(object,parkcode, sitecode, charname,by,assessment,yname,xname,labels,title,assesscolor,outliercolor,sizes,webplot,...){
+   PlotData<-getWData(object=object,parkcode=parkcode, sitecode=sitecode, charname=charname,...)
    if(is.na(yname)) yname<-paste0(getCharInfo(object=object, charname=charname, info="DisplayName")," (",
                                 getCharInfo(object=object, charname=charname, info="Units"),")") %>% unique
    if(is.na(xname)) xname<-switch(by,
@@ -49,13 +55,18 @@ setMethod(f="waterbox", signature=c(object="NCRNWaterObj"),
                           park="Park"
                     )
    
-   if(points) points<-c(getCharInfo(object=object,charname=charname, 
-                                    info="LowerPoint"),getCharInfo(object=object,charname=charname, info="UpperPoint")) %>% unlist %>% unique
-   waterbox(object=PlotData,by=by,points=points,yname=yname,xname=xname,labels=labels,title=title,webplot=webplot)
+   if(assessment) assessment<-c(getCharInfo(object=object,parkcode=parkcode, sitecode=sitecode, charname=charname, info="LowerPoint"),
+                              getCharInfo(object=object,parkcode=parkcode, sitecode=sitecode, charname=charname, info="UpperPoint")) %>%
+       unlist %>% unique
+   
+   assessment<-assessment[!is.na(assessment)] # needed if there is no upper or lower assessment.
+   
+   waterbox(object=PlotData,by=by,assessment=assessment,yname=yname,xname=xname,labels=labels,title=title,assesscolor=assesscolor,
+            outliercolor=outliercolor, sizes=sizes, webplot=webplot)
 })
 
 setMethod(f="waterbox", signature=c(object="data.frame"),
-          function(object,by,points,yname,xname,labels,title,webplot){
+          function(object,by,assessment,yname,xname,labels,title,assesscolor,outliercolor, sizes, webplot){
             Grouper<-switch(by,
                             year=object$Date %>% year %>% factor,
                             month=object$Date %>% month(label=T) %>% factor,
@@ -70,8 +81,8 @@ setMethod(f="waterbox", signature=c(object="data.frame"),
                             park=object$Park %>% unique)
             
             OutPlot<-ggplot(object,aes(Grouper,Value)) +
-              geom_boxplot() +
-              {if (is.numeric(points)) geom_hline(yintercept=points,color="red",linetype="dashed",size=1.1)}+
+              geom_boxplot(outlier.size=sizes[1], outlier.color=outliercolor, lwd=sizes[2]) +
+              {if (is.numeric(assessment)) geom_hline(yintercept=assessment,color=assesscolor,linetype="dashed",size=sizes[3])}+
               labs(title=title,y=yname)+
               scale_x_discrete(name=xname,labels=labels)+
               theme_bw()+
