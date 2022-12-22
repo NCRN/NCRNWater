5757b4f0613b4f6a155f023508c563feac95a5be
cwainright-nps <charles_wainright@nps.gov>
2022-12-22 13:50:35 -0500

    updated read.csv() to data.table::fread(), documented metadata column iterations (e.g., col 1 of 22, 2 of 22...), paused NCRNWater vignette development until upstream water data deliverable format finalized

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -16,14 +16,16 @@ set.seed(1)
 # metadata
 ##############################
 # the original, real metadata
-metadata <- read.csv("water_data/metadata.csv") # directory excluded from github via .gitignore
+metadata <- data.table::fread("water_data/metadata.csv") # directory excluded from github via .gitignore
 metadata_colnames <- colnames(metadata) # save colnames as vector for use later
-# $Network
-length(unique(metadata$Network))
+# 1/22 $Network
+# head(metadata$Network)
+# length(unique(metadata$Network))
+# unique(metadata$Network)
 metadata$Network <- do.call(paste0, Map(stri_rand_strings, n=1, length=c(4),
                                         pattern = c('[A-Z]'))) # replace all $Network values with 4-letter random string
 
-# $ParkCode, ShortName, LongName
+# 2/22, 3/22, 4/22 $ParkCode, $ShortName, $LongName
 # anonymize parks; replace each unique park identifier with a random string
 # create a lookup table to cross-reference random strings against real strings
 n_parks <- length(unique(metadata$ParkCode))
@@ -52,7 +54,7 @@ setnames(metadata, "LongName2", "LongName")
 # put columns back in their original order
 metadata <- metadata %>% select(metadata_colnames)
 
-# $SiteCode, SiteCodeWQX, SiteName
+# 5/22, 6/22, 7/22 $SiteCode, $SiteCodeWQX, $SiteName
 # anonymize sites; replace each unique site identifier with a random strings
 n_sites <- length(unique(metadata$SiteCode))
 lookup <- data.frame(SiteCode = unique(metadata$SiteCode),
@@ -66,11 +68,8 @@ lookup <- data.frame(SiteCode = unique(metadata$SiteCode),
                                        do.call(paste0, Map(stri_rand_strings,
                                                            n=n_sites,
                                                            length=c(4),
-                                                           pattern = c('[A-Z]')
-                                                           )
-                                               ),
-                                       sep = "_"
-                                       ),
+                                                           pattern = c('[A-Z]'))
+                                               ), sep = "_"),
                      SiteCodeWQX = unique(metadata$SiteCodeWQX),
                      SiteCodeWQX2 = "11PPPWRD_WQX",
                      SiteName = unique(metadata$SiteName))
@@ -95,7 +94,7 @@ setnames(metadata, "SiteName2", "SiteName")
 # put columns back in their original order
 metadata <- metadata %>% select(metadata_colnames)
 
-# $Lat, Long
+# 8/22, 9/22 $Lat, $Long
 # anonymize sites; replace lat & lon with rnorm() values
 head(metadata$Lat)
 lookup <- data.frame(Lat = unique(metadata$Lat),
@@ -113,7 +112,80 @@ setnames(metadata, "Lat2", "Lat")
 setnames(metadata, "Long2", "Long")
 # put columns back in their original order
 metadata <- metadata %>% select(metadata_colnames)
-# $AssessmentDetails
+
+# # 10/22 $Type
+# head(metadata$Type)
+# length(unique(metadata$Type))
+# unique(metadata$Type)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 11/22 $CharacteristicName
+# head(metadata$CharacteristicName)
+# length(unique(metadata$CharacteristicName))
+# unique(metadata$CharacteristicName)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 12/22 $DisplayName
+# head(metadata$DisplayName)
+# length(unique(metadata$DisplayName))
+# unique(metadata$DisplayName)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 13/22 $DataName
+# head(metadata$DataName)
+# length(unique(metadata$DataName))
+# unique(metadata$DataName)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 14/22 $Category
+# head(metadata$Category)
+# length(unique(metadata$Category))
+# unique(metadata$Category)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 15/22 $CategoryDisplay
+# head(metadata$CategoryDisplay)
+# length(unique(metadata$CategoryDisplay))
+# unique(metadata$CategoryDisplay)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 16/22 $Units
+# head(metadata$Units)
+# length(unique(metadata$Units))
+# unique(metadata$Units)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 17/22 $LowerPoint
+# head(metadata$LowerPoint)
+# length(unique(metadata$LowerPoint))
+# unique(metadata$LowerPoint)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 18/22 $UpperPoint
+# head(metadata$UpperPoint)
+# length(unique(metadata$UpperPoint))
+# unique(metadata$UpperPoint)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 19/22 $DataType
+# head(metadata$DataType)
+# length(unique(metadata$DataType))
+# unique(metadata$DataType)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 20/22 $LowerDescription
+# head(metadata$LowerDescription)
+# length(unique(metadata$LowerDescription))
+# unique(metadata$LowerDescription)
+# # this is not an identifying field, so we leave as-is
+# 
+# # 21/22 $UpperDescription
+# head(metadata$UpperDescription)
+# length(unique(metadata$UpperDescription))
+# unique(metadata$UpperDescription)
+# # this is not an identifying field, so we leave as-is
+
+# 22/22 $AssessmentDetails
 lookup <- data.frame(AssessmentDetails = unique(metadata$AssessmentDetails))
 for (i in 1:length(lookup$AssessmentDetails)){
   lookup$AssessmentDetails2[i] <- ifelse(lookup$AssessmentDetails[i] == "",
@@ -129,51 +201,42 @@ setnames(metadata, "AssessmentDetails2", "AssessmentDetails")
 # put columns back in their original order
 metadata <- metadata %>% select(metadata_colnames)
 
+# write new metadata to NCRNWater package
 usethis::use_data(metadata, overwrite = TRUE, compress = "xz")
 
 ##############################
 # water data
 ##############################
 # the original, real data
-waterdata <- read.csv("water_data/waterdata.csv") # directory excluded from github via .gitignore
+waterdata <- data.table::fread("water_data/waterdata.csv") # directory excluded from github via .gitignore
 waterdata_colnames <- colnames(waterdata) # save colnames as vector for use later
 
-# $OrganizationIdentifier
+# 1/81 $OrganizationIdentifier
 head(waterdata$OrganizationIdentifier)
 length(unique(waterdata$OrganizationIdentifier))
 # waterdata$OrganizationIdentifier should match the beginning of metadata$SiteCodeWQX
 # regex to extract the string we need
-# extract a string that matches these criteria:
+# extract a string that matches this pattern:
 # 1) string starts with (^) two {2} characters [0-9]. Next,
 # 2) string contains six {6} characters [A-Z]. Next,
 # 3) string contains one {1} underscore '_'. Next,
 # 4) string contains three {3} characters [A-Z].
 waterdata$OrganizationIdentifier <- stringr::str_extract(unique(metadata$SiteCodeWQX)[1], "(^[0-9]{2}[A-Z]{6}_{1}[A-Z]{3})") 
 
-# $OrganizationFormalName
+# 2/81 $OrganizationFormalName
 head(waterdata$OrganizationFormalName)
 length(unique(waterdata$OrganizationFormalName))
-# this is not an identifying piece of data, so we leave as-is
+# this is not an identifying field, so we leave as-is
 
-# $ActivityIdentifier
+# 3/81 $ActivityIdentifier
 head(waterdata$ActivityIdentifier)
-# concatenation:
-### 1) waterdata$OrganizationIdentifier
-### 2) "-"
-### 3) lookup_wqx to cross-ref
-### 4) "_"
-### 5) YYYMMDD date of waterdata$ActivityStartDate
-### 6) ?? is this a timestamp?
-
 # 1) create dummy variable waterdata$SiteCodeWQX
-waterdata$SiteCodeWQX <- NA
 # 2) regex to extract characters from waterdata$ActivityIdentifier into waterdata$SiteCodeWQX
 waterdata$SiteCodeWQX <- stringr::str_extract(waterdata$ActivityIdentifier, "(^[0-9]{2}[A-Z]{6}_{1}[A-Z]{3}-{1}[A-Z]{4}_[A-Z]{4}_[A-Z0-9]{4})")
 # testdf <- waterdata %>% # find any $ActivityIdentifier that the regex couldn't parse
 #   select(ActivityIdentifier, SiteCodeWQX) %>%
 #   subset(is.na(SiteCodeWQX))
 # 3) create dummy variable waterdata$activity_dummy_keep
-waterdata$activity_dummy_keep <- NA
 # 4) regex to extract date string from waterdata$ActivityIdentifier into waterdata$activity_dummy_keep
 waterdata$activity_dummy_keep <- stringr::str_extract(waterdata$ActivityIdentifier, "([0-9]+$)|([0-9]+_{1}[0-9].$)|([0-9]+_{1}[A-Z]+$)|([0-9]+/[0-9]+$)")
 # testdf <- waterdata %>% # find any $ActivityIdentifier that the regex couldn't parse
@@ -184,13 +247,60 @@ waterdata$ActivityIdentifier <- NULL
 # 6) left join waterdata$activity_dummy_match to lookup_wqx$SiteCodeWQX2
 # metadata <- dplyr::left_join(waterdata, lookup_wqx, by.x = "activity_dummy_match", by.y = "SiteCodeWQX2")
 testdf <- dplyr::left_join(waterdata, lookup_wqx %>% select(SiteCodeWQX, SiteCodeWQX2), by = "SiteCodeWQX") %>%
-  select(SiteCodeWQX, SiteCodeWQX2) %>%
-  subset(is.na(SiteCodeWQX2))
-# 7) waterdata$activity_dummy_match <- NULL
-# 8) setnames(waterdata, "SiteCodeWQX2", "ActivityIdentifier")
-# 9) paste0(waterdata$ActivityIdentifier, "_", waterdata$activity_dummy_keep)
-# 10) waterdata$activity_dummy_keep <- NULL
-# 11) waterdata <- waterdata %>% select(waterdata_colnames)
+  dplyr::select(SiteCodeWQX, SiteCodeWQX2) %>%
+  dplyr::subset(is.na(SiteCodeWQX2)) %>%
+  dplyr::distinct(SiteCodeWQX, .keep_all = TRUE) %>%
+  dplyr::rename(SiteCodeWQX_in_waterdata = SiteCodeWQX,
+                SiteCodeWQX_in_metadata = SiteCodeWQX2)
+# !!!! STOPPED HERE UNTIL METADATA ISSUE IS RESOLVED https://github.com/NCRN/NCRN_DM/issues/92
+# 7) delete $activity_dummy_match
+waterdata$activity_dummy_match <- NULL
+# 8) rename 
+dat.table::setnames(waterdata, "SiteCodeWQX2", "ActivityIdentifier")
+# 9) concatenate
+paste0(waterdata$ActivityIdentifier, "_", waterdata$activity_dummy_keep)
+# 10) delete $activity_dummy_keep
+waterdata$activity_dummy_keep <- NULL
+# 11) re-order columns
+waterdata <- waterdata %>% select(waterdata_colnames)
+
+# 4/81 $ActivityTypeCode
+head(waterdata$ActivityTypeCode)
+length(unique(waterdata$ActivityTypeCode))
+# this is not an identifying field, so we leave as-is
+
+# 5/81 $ActivityMediaName
+head(waterdata$ActivityMediaName)
+length(unique(waterdata$ActivityMediaName))
+unique(waterdata$ActivityMediaName)
+# this is not an identifying field, so we leave as-is
+
+# 6/81 $ActivityStartDate
+head(waterdata$ActivityStartDate)
+length(unique(waterdata$ActivityStartDate))
+# this is not an identifying field, so we leave as-is
+
+# 7/81 $`ActivityStartTime/Time`
+head(waterdata$`ActivityStartTime/Time`)
+length(unique(waterdata$`ActivityStartTime/Time`))
+# this is not an identifying field, so we leave as-is
+
+# 8/81 $`ActivityStartTime/TimeZoneCode`
+head(waterdata$`ActivityStartTime/TimeZoneCode`)
+length(unique(waterdata$`ActivityStartTime/TimeZoneCode`))
+# this is not an identifying field, so we leave as-is
+
+# 9/81 $ActivityEndDate
+head(waterdata$ActivityEndDate)
+length(unique(waterdata$ActivityEndDate))
+# this is not an identifying field, so we leave as-is
+
+
+
+
+
+
+
 
 
 
